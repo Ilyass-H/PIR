735cf37a65044dd0ccb0de6a50a02b396bc7984a
Ilyass HAMMOUAMRI <Hammouamri.ilyass@gmail.com>
2019-04-24 18:30:20 +0100

    update

--- test.py
+++ test.py
@@ -8,24 +8,28 @@ import load
 MEAN = 26331.311502
 STD = 1799.268739
 
-N_TEST = 100
-N_Train = 5000
+N_TEST = 10279
+N_Train = 21896
 
 
 file = open("bestHyperParam.json","r")
 hyperParam = json.load(file)
 
+print("Loading Test Data ...\n")
 X_test,Y_test,massPast = load.load_data(N_TEST,'scaledTestData.csv',"test")
+print("Loading Training Data ...\n")
 X,Y = load.load_data(N_Train,'scaledTrainTestData.csv')
 
 hyperParam = json.dumps({"LR":hyperParam["LR"],"H_SIZE":hyperParam["H_SIZE"],"EPOCHS":hyperParam["EPOCHS"],"LAYERS":hyperParam["LAYERS"]})
+
+print("Start Training ... \n")
 pred = train.train(X,Y,hyperParam,X_test,Y_test,"test")
 
 
 pred_loss = 0
 bada_loss = 0
 
-cnt = 10
+cnt = 3
 
 print("\n\n")
 for j in range(len(pred)):
@@ -35,9 +39,13 @@ for j in range(len(pred)):
 				print(((pred[j][i].item() * STD)+MEAN),end=' - ')
 				print(((Y_test[j][i][0].item() * STD)+MEAN),end=", massPast = ")
 				print(massPast[j][i][0])
-				cnt -= 1
+				
 			pred_loss += (((pred[j][i].item() * STD)+MEAN) - ((Y_test[j][i][0] * STD)+MEAN))**2
 			bada_loss += (massPast[j][i][0] - ((Y_test[j][i][0] * STD)+MEAN))**2
+	if cnt > 0:
+		print("\n######\n")
+		cnt-=1
+
 
 print("\n# pred_loss = ",np.sqrt(pred_loss.cpu()))
 print("# bada_loss = ",np.sqrt(bada_loss.cpu()))
